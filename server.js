9eb8a1f3d8db3e0d627a02e42c4a6b66efa3751b
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2013-09-06 18:03:14 -0400

    Added default app settings.

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ amqp = require('amqp');
 
 var app = express();
 
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.connectionStatus = 'No server connection';
+app.exchangeStatus = 'No exchange established';
+app.queueStatus = 'No queue established';
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log("RabbitMQ + Node.js app running on AppFog!");
 });
